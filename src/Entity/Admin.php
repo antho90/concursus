ab82bc372cdc27d77502ca98286f56baecb1ac25
Anthony Chatelot <anthonychxtelxt@gmail.com>
2021-03-08 09:47:30 +0100

    relation admin et mentor

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AdminRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -26,6 +28,16 @@ class Admin
      * @ORM\Column(type="string", length=255)
      */
     private $password;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Mentor::class, inversedBy="admins")
+     */
+    private $mentor;
+
+    public function __construct()
+    {
+        $this->mentor = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +64,30 @@ class Admin
     public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Mentor[]
+     */
+    public function getMentor(): Collection
+    {
+        return $this->mentor;
+    }
+
+    public function addMentor(Mentor $mentor): self
+    {
+        if (!$this->mentor->contains($mentor)) {
+            $this->mentor[] = $mentor;
+        }
+
+        return $this;
+    }
+
+    public function removeMentor(Mentor $mentor): self
+    {
+        $this->mentor->removeElement($mentor);
 
         return $this;
     }
--- src/Entity/Mentor.php
+++ src/Entity/Mentor.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\MentorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -31,6 +33,16 @@ class Mentor
      * @ORM\Column(type="string", length=255)
      */
     private $nom;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Admin::class, mappedBy="mentor")
+     */
+    private $admins;
+
+    public function __construct()
+    {
+        $this->admins = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -69,6 +81,33 @@ class Mentor
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Admin[]
+     */
+    public function getAdmins(): Collection
+    {
+        return $this->admins;
+    }
+
+    public function addAdmin(Admin $admin): self
+    {
+        if (!$this->admins->contains($admin)) {
+            $this->admins[] = $admin;
+            $admin->addMentor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAdmin(Admin $admin): self
+    {
+        if ($this->admins->removeElement($admin)) {
+            $admin->removeMentor($this);
+        }
 
         return $this;
     }
