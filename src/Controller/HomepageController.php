ab0c784892949c250b2faaa7688ca6dc451d8f47
Anthony Chatelot <anthonychxtelxt@gmail.com>
2021-03-09 15:48:21 +0100

    test url

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Competiton;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,24 +14,54 @@ class HomepageController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index() {
-        return $this->render('homepage/index.html.twig', ['mainNavHome'=>true, 'title'=>'Accueil']);
+    public function index()
+    {
+        return $this->render('homepage/index.html.twig', ['mainNavHome' => true, 'title' => 'Accueil']);
     }
 
     /**
      * @Route("/programme")
      */
-    public function programme(CompetitonRepository $repository) {
+    public function programme(CompetitonRepository $repository)
+    {
 
         $programmes = $repository->findAll();
 
-        return $this->render('homepage/programme.html.twig', ['programmes'=> $programmes, 'title'=>'Programme']);
+        return $this->render('homepage/programme.html.twig', ['programmes' => $programmes, 'title' => 'Programme']);
     }
 
     /**
      * @Route("/competition")
      */
-    public function competition() {
-        return $this->render('homepage/competition.html.twig', ['title'=>'Competition']);
+    public function competition()
+    {
+        return $this->render('homepage/competition.html.twig', ['title' => 'Competition']);
     }
+
+    /**
+     * @Route("/competition/{id}", name="competition")
+     */
+
+
+    public function showCompetiton($id)
+    {
+        $repo = $this->getDoctrine()->getRepository(Competiton::class);
+        $competition = $repo->find($id);
+        return $this->render('homepage/competition.html.twig', [
+            'competition' => $competition
+        ]);
+    }
+
+    // public function showCompetiton($id)
+    // {
+    //     $competition = $this->getDoctrine()
+    //         ->getRepository('App:Competiton')
+    //         ->find($id);
+
+    //     if (!$competition) {
+    //         throw $this->createNotFoundException('Aucune compétition ne correspond à l\'id');
+    //     }
+
+    //     return $this->render('homepage/competition.html.twig', ['title' => 'Competition']);
+    // }
 }
