31b441a8c6b8dc7c5ce3ed82a79104291cfcfa7b
Anthony Chatelot <anthonychxtelxt@gmail.com>
2021-03-30 16:52:52 +0200

    modifier équipes pour mentor

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -196,7 +196,7 @@ class HomepageController extends AbstractController
      * @IsGranted("ROLE_ADMIN")
      * @Route("/equipes/modifier/{id}", name="modifier_equipe")
      */
-    public function editEquipe(Equipe $equipe, Competiton $competition, Request $request){
+    public function editEquipe(Equipe $equipe, Request $request){
         $form = $this->createForm(EditEquipeType::class, $equipe);
         $form->handleRequest($request);
 
@@ -208,6 +208,29 @@ class HomepageController extends AbstractController
 
             $this->addFlash('success', 'Equipe modifié avec succès');
             return $this->redirectToRoute('equipes');
+        }
+
+        return $this->render('admin/editequipe.html.twig', [
+            'equipeForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_MENTOR")
+     * @Route("/mentor/equipes/modifier/{id}", name="modifier_equipe")
+     */
+    public function mentoreditEquipe(Equipe $equipe, Request $request){
+        $form = $this->createForm(EditEquipeType::class, $equipe);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager ->persist($equipe);
+           // $entityManager ->persist($competition);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Equipe modifié avec succès');
+            return $this->redirectToRoute('mentor_equipes');
         }
 
         return $this->render('admin/editequipe.html.twig', [
